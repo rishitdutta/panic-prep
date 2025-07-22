1038aa2df9be8d9fdb982ce755b19167f7bde6ce
Rishit Dutta <rishitdutta03@gmail.com>
2025-07-22 12:37:39 +0530

    first push

---
+++ app/form-actions.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { z } from "zod";
+import { promptSchema } from "@/lib/validator";
+
+export interface ServerResult {
+  success: boolean;
+  message: string;
+  errors?: Record<string, string[] | undefined>;
+  submittedValue?: string;
+}
+
+export async function handleSubmit(
+  prevState: ServerResult,
+  formData: FormData
+): Promise<ServerResult> {
+  const rawPrompt = formData.get("prompt") as string;
+  const toValidate = { prompt: rawPrompt };
+  const validated = promptSchema.safeParse(toValidate);
+  if (!validated.success) {
+    const fieldErrors = validated.error.flatten().fieldErrors;
+    console.log("Server side validation errors", fieldErrors);
+    return {
+      success: false,
+      message: "Validation failed",
+      errors: { prompt: fieldErrors.prompt },
+    };
+  }
+  const validPrompt = validated.data.prompt;
+  console.log("Validation successful, Prompt:", validPrompt);
+  //send to api
+  /* try{
+    const apiResponse = await fetch("api-url",{
+      method="POST",
+      body= JSON.stringify({userPrompt: validPrompt})
+    });
+    if(!apiResponse.ok) throw new Error("API Submission failed");
+  } catch (apiResponse) {
+    return {success:false, message: "Failed to submit to external API."}
+  } */
+
+  //dummy return
+  return {
+    success: true,
+    message: "Form submitted successfully",
+    submittedValue: validPrompt,
+  };
+}
